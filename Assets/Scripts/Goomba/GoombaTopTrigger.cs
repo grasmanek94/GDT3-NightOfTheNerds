5770b69fb9e3a4ef584c2cefef4b985dba23775f
Gamer_Z <grasmanek94@gmail.com>
2017-12-21 13:20:24 +0100

    add sounds for goomba and player

--- Assets/Scripts/Goomba/GoombaTopTrigger.cs
+++ Assets/Scripts/Goomba/GoombaTopTrigger.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class GoombaTopTrigger : MonoBehaviour {
 
+    [SerializeField]
+    AudioSource[] audio_sources;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
             other.GetComponent<Player>().Jump();
-            this.GetComponentInParent<AudioSource>().Play();
+            int index = Random.Range(1, audio_sources.Length);
+            AudioSource source = audio_sources[index];
+            AudioSource zero = audio_sources[0];
+            audio_sources[0] = source;
+            audio_sources[index] = zero;
+            source.Play();
 			this.GetComponentInParent<SpriteRenderer>().flipY = true;
             this.GetComponentInParent<Goomba>().dead = true;
             Destroy(this.GetComponentInParent<Goomba>().gameObject, 1);
