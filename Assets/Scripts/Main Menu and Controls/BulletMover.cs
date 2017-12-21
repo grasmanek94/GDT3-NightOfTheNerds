c9d4dcf2b4a15c9a67311b5dac77d6e3b2f298ec
harmE1997 <harmelfring@live.nl>
2017-12-21 11:37:51 +0100

    Harm's integration
    Added Main menu and controls test scene to project

---
+++ Assets/Scripts/Main Menu and Controls/BulletMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour {
+
+    public GameObject player;
+    public int bulletSpeed = 5;
+    public float timer = 0;
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+        if (timer < 2)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * bulletSpeed);
+        }
+
+        else
+        {
+            gameObject.SetActive(false);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Main Menu and Controls/UIControl.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIControl : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject LevelSelector;
+    public GameObject playLevelBtn;
+    public Image Level1IMG;
+    public Image Level2IMG;
+    public Image SelectedLevel;
+    public Text Level1txt;
+    public Text Level2txt;
+    public Text SelectedLevelText;
+    private string SceneName;
+
+	void Start()
+    {
+        MainMenu.SetActive(true);
+        LevelSelector.SetActive(false);
+        Level1IMG.color = new Color(255, 255, 255, 50);
+        Level1IMG.GetComponent<Button>().interactable = false;
+        Level1txt.text = "locked";
+        Level2IMG.color = new Color(255, 255, 255, 50);
+        Level2IMG.GetComponent<Button>().interactable = false;
+        Level2txt.text = "locked";
+        playLevelBtn.SetActive(false);
+	}
+	
+	void Update()
+    {
+		
+	}
+
+    public void ev_ExitGameBtn()
+    {
+        Debug.Log("exitting the game");
+        Application.Quit();
+    }
+
+    public void ev_StartGameBtn()
+    {
+        MainMenu.SetActive(false);
+        LevelSelector.SetActive(true);
+    }
+
+    public void ev_ReturnToMainBtn()
+    {
+        LevelSelector.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+
+    public void ev_PlayLevelBtn()
+    {
+        if (!string.IsNullOrEmpty(SceneName))
+        {
+            SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+        }     
+    }
+
+    public void SelectLevel(Image level)
+    {
+        SelectedLevel.color = Color.white;
+        SelectedLevel.sprite = level.sprite;
+        playLevelBtn.SetActive(true);
+        SceneName = level.GetComponentInChildren<Text>().text;
+    }
+
+    public void UnlockLevel(string name)
+    {
+        if (name == "Level 1")
+        {
+            Level1IMG.color = Color.white;
+            Level1IMG.GetComponent<Button>().interactable = true;
+            Level1txt.text = "Level 1";
+        }
+
+        else if (name == "Level 2")
+        {
+            Level2IMG.color = Color.white;
+            Level2IMG.GetComponent<Button>().interactable = true;
+            Level2txt.text = "Level 2";
+        }
+    }
+}
