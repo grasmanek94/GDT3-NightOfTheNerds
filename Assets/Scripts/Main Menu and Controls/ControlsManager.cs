443453167370d12386de26cdb2e0bbe789dca13e
Gijs <g.vandenbiggelaar@gmail.com>
2017-12-22 11:52:01 +0100

    Minor fix in ControlsManager

--- Assets/Scripts/Main Menu and Controls/ControlsManager.cs
+++ Assets/Scripts/Main Menu and Controls/ControlsManager.cs
@@ -6,39 +6,37 @@ public class ControlsManager : MonoBehaviour
 {
     public GameObject playerObject;
     private Player player;
-	void Start()
+    void Start()
     {
         Input.multiTouchEnabled = true;
         player = playerObject.GetComponent<Player>();
     }
-	
-	void Update()
+
+    void Update()
     {
- 
+
     }
 
     public void OnSubmit(string action)
     {
-        if (action == "jump")
+        if (!player.dead)
         {
-            if (!player.dead)
+            if (action == "jump")
             {
-                if (action == "jump")
-                {
-                    jump();
-                }
+                jump();
+            }
 
-                else if (action == "shoot")
+            else if (action == "shoot")
+            {
+                if (player.type == Player.PlayerType.Shooter && player.fired == false)
                 {
-                    if (player.type == Player.PlayerType.Shooter && player.fired == false)
-                    { 
-                       player.Shoot();
-                       player.fired = true;                        
-                    }
+                    player.Shoot();
+                    player.fired = true;
                 }
             }
-         }   
-     }
+        }
+
+    }
     public void jump()
     {
         if (player.isGrounded == true && player.jumped == false)
