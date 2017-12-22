ff0978dfd1e93455225f59734bc67eaaed13b883
Gijs <g.vandenbiggelaar@gmail.com>
2017-12-22 11:39:36 +0100

    Fixed player being able to move after death

--- Assets/SimpleTouchController/Scripts/PlayerMoveController.cs
+++ Assets/SimpleTouchController/Scripts/PlayerMoveController.cs
@@ -8,35 +8,33 @@ public class PlayerMoveController : MonoBehaviour {
 	public float speedMovements = 5;
 
 	// PRIVATE
-	private Rigidbody _rigidbody;
     private GameObject player;
 
 	void Awake()
 	{
-		_rigidbody = GetComponent<Rigidbody>();
         player = gameObject.GetComponent<ControlsManager>().playerObject;
 	}
 
 	void FixedUpdate()
 	{
-		// move
-		//_rigidbody.MovePosition(transform.position + (transform.forward * leftController.GetTouchPosition.y * Time.deltaTime * speedMovements) +
-		//	(transform.right * leftController.GetTouchPosition.x * Time.deltaTime * speedMovements) );
+        if (!player.GetComponent<Player>().dead)
+        {
+            if (leftController.GetTouchPosition.x > 0)
+            {
+                player.GetComponent<Player>().moveDirection = Player.Direction.Right;
+                player.GetComponent<Player>().isMoving = true;
+            }
+            else if (leftController.GetTouchPosition.x < 0)
+            {
+                player.GetComponent<Player>().moveDirection = Player.Direction.Left;
+                player.GetComponent<Player>().isMoving = true;
+            }
+            else if (leftController.GetTouchPosition.x == 0)
+            {
+                player.GetComponent<Player>().isMoving = false;
+            }
 
-        if (leftController.GetTouchPosition.x > 0)
-        {
-            player.GetComponent<Player>().moveDirection = Player.Direction.Right;
-            player.GetComponent<Player>().isMoving = true;
+            player.transform.Translate(new Vector2(player.GetComponent<Player>().walkSpeed * leftController.GetTouchPosition.x / 10, 0));
         }
-        else if (leftController.GetTouchPosition.x < 0)
-        {
-            player.GetComponent<Player>().moveDirection = Player.Direction.Left;
-            player.GetComponent<Player>().isMoving = true;
-        }
-        else if (leftController.GetTouchPosition.x == 0)
-        {
-            player.GetComponent<Player>().isMoving = false;
-        }
-        player.transform.Translate(new Vector2(player.GetComponent<Player>().walkSpeed * leftController.GetTouchPosition.x / 10, 0));
     }
 }
